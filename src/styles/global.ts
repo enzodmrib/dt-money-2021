455ed076e29d46316100f1a1db29bc88e69c2286
enzodmrib <enzorib2@gmail.com>
2022-07-03 16:51:09 -0300

    Adds font importation and applies to global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -17,7 +17,7 @@ export const GlobalStyle = createGlobalStyle`
     * {
         margin: 0;
         padding: 0;
-        box-sizing: border-box;
+        box-sizing: bo rder-box;
     }
 
     // font-size: 16px; --> standard
@@ -40,6 +40,15 @@ export const GlobalStyle = createGlobalStyle`
     body {
         background: var(--background);
         -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button{
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5 , h6, strong {
+        font-weight: 600;
     }
 
     button {
